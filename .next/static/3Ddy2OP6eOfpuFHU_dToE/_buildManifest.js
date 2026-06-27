e1e5f54d805d07cffe81236091943d9e4eb5ccaf
Davey Wong <wgwcko@gmail.com>
2026-06-28 00:42:08 +0800

    chore: 清理 .next 构建产物 + 更新 .gitignore

--- .next/static/3Ddy2OP6eOfpuFHU_dToE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-47a9f13b53e9c2e7.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
